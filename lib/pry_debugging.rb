b832fb5bd553af182b3451c7de7cf54b188340ca
Emma Cohanim <emmacohanim@gmail.com>
2022-08-29 10:40:16 -0700

    commit tree 378b87232e48307941a441a23f8a6654e6ae8ae5

--- lib/pry_debugging.rb
+++ lib/pry_debugging.rb
@@ -1,4 +1,13 @@
+
+# Binding
+# -built-in class that takes objects in context of current scope and holds them for use outside
+
+# binding.pry
+# -pries into current context of code outside of file
+# -similar to adding debugger
+# --> set breakpoint in code
+
 def plus_two(num)
-	num + 2
-	num
+    num=num + 2
+    num
 end
